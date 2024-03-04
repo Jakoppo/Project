5f708f7cadd46960dfba823e1a385ce351eac0bd
Jakub <profiljakub@gmail.com>
2024-03-04 13:07:46 +0100

    logowanie i rejestracaja

--- log.php
+++ log.php
@@ -4,17 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/stylelog.css">
 </head>
 <body>
   <div class="container">
     <form class="login-form">
       <h2>Login</h2>
       <input type="text" placeholder="Username" required>
-      <br>
       <input type="password" placeholder="Password" required>
       <button type="submit">Login</button>
-      <p>Don't have an account? <a href="register.html">Register here</a></p>
+      <p>Don't have an account? <a href="pages/rejestr.php">Register here</a></p>
     </form>
   </div>
 </body>
