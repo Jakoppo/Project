4c40713888d8d39c149ce55f7f39de369bfb4855
Jakub <profiljakub@gmail.com>
2024-03-04 12:34:18 +0100

    new files added

---
+++ log.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <form class="login-form">
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" required>
+      <br>
+      <input type="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+      <p>Don't have an account? <a href="register.html">Register here</a></p>
+    </form>
+  </div>
+</body>
+</html>
